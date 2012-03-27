5abe0765a446703f3e2d999af1fa0fcf438dc9ab
Charlie Powell <powellcj12@gmail.com>
2012-03-26 21:58:46 -0400

    cleaned up button code; interrupts work better with 67nF caps

--- button/button.ino
+++ button/button.ino
@@ -1,11 +1,5 @@
-// constants won't change. They're used here to 
-// set pin numbers:
-const int buttonPin = 14;     // the number of the pushbutton pin
+const int buttonPin = 14;     // analog pins start at 14
 const int ledPin =  13;      // the number of the LED pin
-
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-volatile char keyPress = 0x0;
 
 void setup() {
   PCICR |= 1 << PCIE1; //enable interups for Port C
@@ -22,42 +16,11 @@ void setup() {
   
   DDRC = B00000000; //all pins are input (only use [5:0])
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
   interrupts();
 }
 
 void loop(){
-  // read the state of the pushbutton value:
-  //buttonState = digitalRead(buttonPin);
-  /*
-  buttonState = keyPress & B00100000;
-  
-  for(int i = 0; i < 6; i++)
-  {
-      //int val = keyPress >> i;
-      char pinTest = 1 << i;
-      
-      if(keyPress & pinTest)
-      {
-        Serial.print("Button ");
-        Serial.println(i);
-        //Serial.print(": ");
-        //Serial.println(val);
-      }
-  }
-
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {     
-    // turn LED on:    
-    digitalWrite(ledPin, HIGH); 
-  } 
-  else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW); 
-  }
-  */
 }
 
 ISR(PCINT1_vect) {
@@ -73,6 +36,4 @@ ISR(PCINT1_vect) {
       Serial.println("Pin 1");
   if(PINC & B00000001)
       Serial.println("Pin 0");
-    
-  //keyPress = PINC;
 }
