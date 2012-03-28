1f187c68bd7b009cbd62a9475e5786f4dceff6b7
Charlie Powell <powellcj12@gmail.com>
2012-03-28 16:34:55 -0400

    merge buttons into slave

--- slave/slave.ino
+++ slave/slave.ino
@@ -6,17 +6,34 @@
 
 //PIN Setup
 //B[5:0] Data
+//C[5:0] Buttons
 //D[7:6] Control
 //D[5] ACK
 //D[1:0] Select Lines
 #define BUFFERSIZE 20
+const int buttonPin = 14; // analog pins start at 14
+
 void setup()
 {
     //Serial.begin(115200);
     DDRD = (B11000011 | DDRD) & B11011111;
     DDRB = (B00111111 | DDRB);
+    DDRC = (B00000000 | DDRC);
+    
+    PCICR |= 1 << PCIE1; //enable interups for Port C
+  
+    //watch for change on all pins of Port C
+    PCMSK1 |= 1 << PCINT8;
+    PCMSK1 |= 1 << PCINT9;
+    PCMSK1 |= 1 << PCINT10;
+    PCMSK1 |= 1 << PCINT11;
+    PCMSK1 |= 1 << PCINT12;
+    PCMSK1 |= 1 << PCINT13;
+  
+    MCUCR = (1<<ISC01) | (1<<ISC01); //risign edge triggered
     
     randomSeed(analogRead(0));
+    interrupts();
 }
 
 char ackMask = B00100000;
@@ -58,7 +75,6 @@ char state = 0x00;
 
 void loop()
 {
-     
     PORTB = 0x00;
     
     while (1)
@@ -112,4 +128,19 @@ void loop()
             }
         }
     }
+}
+
+ISR(PCINT1_vect) {
+  if(PINC & B00100000)
+      Serial.println("Pin 5");
+  if(PINC & B00010000)
+      Serial.println("Pin 4");
+  if(PINC & B00001000)
+      Serial.println("Pin 3");
+  if(PINC & B00000100)
+      Serial.println("Pin 2");
+  if(PINC & B00000010)
+      Serial.println("Pin 1");
+  if(PINC & B00000001)
+      Serial.println("Pin 0");  
 }
